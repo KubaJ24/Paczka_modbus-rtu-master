c63fab9a2ce7f8879d7547484823f36d0377c861
KubaJ24SIM <j.jarosz@onet.pl>
2025-06-18 11:16:40 +0100

    Utworzenie skryptu do kopiowania paczek; Aktualizacja paczki - pomiar prądu i zmiana portu modbus

---
+++ deb_cp.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Skrypt do przenoszenia paczek do repo
+
+# Zmienne
+PROG_NAME=".deb copier"
+
+echo "${PROG_NAME}"
+
+# Kopiowanie paczki .deb
+cp ~/Desktop/programs/modbus/RPi3B_modbus_for_KALi/modbus-rtu-master-0.9.1.deb pool/main/
+
+# Tworzenie paczek
+# dists/stable...
+dpkg-scanpackages pool /dev/null > dists/stable/main/binary-armhf/Packages
+gzip -f dists/stable/main/binary-armhf/Packages
+dpkg-scanpackages pool /dev/null > dists/stable/main/binary-arm64/Packages
+gzip -f dists/stable/main/binary-arm64/Packages
+# main/dists/stable...
+dpkg-scanpackages pool /dev/null > main/dists/stable/main/binary-armhf/Packages
+gzip -f main/dists/stable/main/binary-armhf/Packages
+dpkg-scanpackages pool /dev/null > main/dists/stable/main/binary-arm64/Packages
+gzip -f main/dists/stable/main/binary-arm64/Packages
+
+echo "Paczki skopiowane do repozytorium 'modbus-rtu-master-repo'"
+
